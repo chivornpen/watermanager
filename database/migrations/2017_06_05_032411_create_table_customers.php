f8e71e5ba705a2f92021ff906fbb2d6ee167a1f9
Sokrann On <rannon888@gmail.com>
2017-06-05 11:00:14 +0700

    Make migration table

---
+++ database/migrations/2017_06_05_032411_create_table_customers.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCustomers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('contactNo');
+            $table->string('email');
+            $table->string('homeNo');
+            $table->string('stringNo');
+            $table->integer('village_id');
+            $table->integer('district_id');
+            $table->integer('commune_id');
+            $table->integer('province_id');
+            $table->string('cusbc')->nullable();
+            $table->string('cusqr')->nullable();
+            $table->integer('user_id');
+            $table->string('cusStatus')->nullable();
+            $table->string('authorizer')->nullable();
+            $table->date('authorize_date')->nullable();
+            $table->string('recordStatus')->nullable();
+            $table->integer('recordNum')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
